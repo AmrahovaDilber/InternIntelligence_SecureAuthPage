6559ad32c111bf4f6b4e0e99b219989e6dcb6b57
AmrahovaDilber <amrahovad911@gmail.com>
2025-01-25 22:47:06 +0400

    firebase changes

--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -2,17 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-
 const firebaseConfig = {
-  apiKey:"AIzaSyB8qZpaqHlar-uYpozGdbXFt4K6e8s2Lms",
-  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.VITE_FIREBASE_APP_ID,
-  measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyB8qZpaqHlar-uYpozGdbXFt4K6e8s2Lms",
+  authDomain: "auth-a6579.firebaseapp.com",
+  projectId: "auth-a6579",
+  storageBucket: "auth-a6579.firebasestorage.app",
+  messagingSenderId: "581359482548",
+  appId: "1:581359482548:web:63083375d64565819909e1",
+  measurementId: "G-XPXWT1Z21H"
 };
-
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
